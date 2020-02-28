91170096fd24c27d97fae5aaa54f5e7f86ab082a
Dorian Isaac Vallecillo Calderón <dorianivc1@gmail.com>
2020-02-27 20:17:30 -0600

    App Ready

---
+++ Try-Best/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Try_Best.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()//RAM
+        {
+            ViewBag.Message = "Your application description page.";
+            About();
+            return View();
+        }
+
+        public ActionResult Contact()//CPU
+        {
+            while (true)
+            {
+                Console.WriteLine("Interrumpcion");
+            }
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Salida()//TCP
+        {
+            while (true)
+            {
+                TcpClient tcpClient = new TcpClient();
+                IPAddress ipAddress = Dns.GetHostEntry("www.google.com").AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 80);
+                tcpClient.Connect(ipEndPoint);
+            }
+          
+
+            return View();
+        }
+    }
+}
